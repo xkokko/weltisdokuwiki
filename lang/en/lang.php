ffe552d527359ba40bbe769061652fd06613785e
Kokko <c.github@drw.ng>
2024-02-21 12:58:59 +0100

    Update lang.php

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -2,11 +2,27 @@
 
 // style.ini values
 
-$lang['__background_site__'] = 'Color for the very background (behind the content box)';
-$lang['__link__']     = 'The general link color';
-$lang['__existing__'] = 'The color for links to existing pages';
-$lang['__missing__']  = 'The color for links to non-existing pages';
+$lang['__background_site__'] = '[UNUSED] Color for the very background (behind the content box)';
+$lang['__link__']     = '[UNUSED] The general link color';
+$lang['__existing__'] = '[UNUSED] The color for links to existing pages';
+$lang['__missing__']  = '[UNUSED] The color for links to non-existing pages';
+
 $lang['__site_width__']    = 'The width of the full site (can be any length unit: %, px, em, ...)';
 $lang['__sidebar_width__'] = 'The width of the sidebar, if any (can be any length unit: %, px, em, ...)';
+
 $lang['__tablet_width__']  = 'Below screensizes of this width, the site switches to tablet mode';
 $lang['__phone_width__']   = 'Below screensizes of this width, the site switches to phone mode';
+
+$lang['__title_font__']     = 'Font for the main title of the wiki';
+$lang['__title_style__']   = 'Casing for the main title of the wiki';
+$lang['__title_size__']     = 'Size for the main title of the wiki';
+$lang['__title_weight__']   = 'Weight for the main title of the wiki';
+
+$lang['__top_color__'] = 'Top of the accent gradient of the theme';
+$lang['__mid_color__'] = 'Middle of the accent gradient of the theme';
+$lang['__bot_color__'] = 'Bottom of the accent gradient of the theme';
+$lang['__alt_color__'] = 'Special accent color of the theme';
+$lang['__top_alpha_color__'] = 'Transparent Top of the accent gradient of the theme';
+$lang['__mid_alpha_color__'] = 'Transparent Middle of the accent gradient of the theme';
+$lang['__bot_alpha_color__'] = 'Transparent Bottom of the accent gradient of the theme';
+$lang['__alt_alpha_color__'] = 'Transparent Special accent color of the theme';
